5d6772d4f61742a2fc24719287c0e4aa862113b0
pooyan <thepooyan@gmail.com>
2022-03-01 13:23:35 +0330

    login and logout compeletely done

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import styles from "./Header.module.scss";
-import { faSearch , faShoppingCart , faUser} from '@fortawesome/free-solid-svg-icons'
+import { faSearch , faShoppingCart , faUser, faSortDown, faSignOutAlt, faShoppingBag, faHeart, faComment, faCircle, faUserCircle} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import '../../fonts/Iranian Sans.ttf'
 import { Link } from "react-router-dom";
+import { LoginContext } from "../../data/GeneralInfo";
 
 const HomePage = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useContext(LoginContext);
+  const [isLoginClicked, setIsLoginClicked] = useState(false);
+
+  const loginClickHandler = () => {
+    setIsLoginClicked(!isLoginClicked)
+  }
+  const logOut = () => {
+    setIsLoggedIn(false)
+  }
+
   return (
     <div>
       <div className={styles.container}>
@@ -17,10 +29,25 @@ const HomePage = () => {
             <FontAwesomeIcon icon={faSearch} className={styles.search}/>
         </div>
         <div className={styles.three}>
+            {!isLoggedIn?
             <Link to='/login' className={styles.signUp}>
                 <FontAwesomeIcon icon={faUser} className={styles.user}/>
                 ورود به حساب کاربری
             </Link>
+            :
+            <div className={styles.loggedIn} onClick={loginClickHandler}>
+              <FontAwesomeIcon icon={faUser} className={styles.user}/>
+              <FontAwesomeIcon icon={faSortDown} className={styles.tic}/>
+              {isLoginClicked&&<div className={styles.extend}>
+                <div className={styles.item}><FontAwesomeIcon icon={faUserCircle} className={styles.mainIcon}/><span className={styles.mainItem}>پویان سلمانی</span></div>
+                  <div className={styles.item}><FontAwesomeIcon className={styles.icon} icon={faCircle}/><span>فعال سازی کیف پول</span></div>
+                  <div className={styles.item}><FontAwesomeIcon className={styles.icon} icon={faHeart}/><span>لیست ها</span></div>
+                  <div className={styles.item}><FontAwesomeIcon className={styles.icon} icon={faShoppingBag}/><span>سفارش ها</span></div>
+                  <div className={styles.item}><FontAwesomeIcon className={styles.icon} icon={faComment}/><span>دیدگاه ها</span></div>
+                <div onClick={logOut} className={`${styles.item} ${styles.lastItem}`}><FontAwesomeIcon className={styles.icon} icon={faSignOutAlt}/><span>خروج از حساب کاربری</span></div>
+              </div>}
+            </div>
+            }
             <div className={styles.border}></div>
             <FontAwesomeIcon icon={faShoppingCart} className={styles.cart}/>
         </div>
