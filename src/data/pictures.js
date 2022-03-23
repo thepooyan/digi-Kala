9ca51bb1e480a283185c1a0bd4d9e28cdacdf68b
pooyan <thepooyan@gmail.com>
2022-03-23 12:30:43 +0430

    cards made out of redux store(store is local)

--- src/data/pictures.js
+++ src/data/pictures.js
@@ -1,3 +1,8 @@
 export const pic = {
     iphone12: 'https://dkstatics-public.digikala.com/digikala-products/be7a0e9bf7866759fa3cea7648b149f589a01040_1607433995.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80',
+    galaxyA12: 'https://dkstatics-public.digikala.com/digikala-products/c5c4a0bb88dc312a4c2f6de0b1567ef0da31a2d9_1630140406.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80',
+    iphone13proMAX: 'https://dkstatics-public.digikala.com/digikala-products/15d774761e234a15c1c559ac540963baad9e229f_1631704695.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80',
+    redmiA9: 'https://dkstatics-public.digikala.com/digikala-products/2f5cbd1996ddd89464afeed70d4def6529cb4ed8_1597575940.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80',
+    iphone11: 'https://dkstatics-public.digikala.com/digikala-products/987bbf684de2df847c8fa1dcb9570fbf580f79dc_1611468226.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80',
+    galaxyA03: 'https://dkstatics-public.digikala.com/digikala-products/c534b55b28b295db460f2bf8ab0f476bb1e3be0b_1640519213.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80',
 }
--- src/redux/productsReducer.js
+++ src/redux/productsReducer.js
@@ -2,9 +2,11 @@ import { pic } from "../data/pictures"
 
 const INITIAL_STATE = [
     {id:1, name:'Iphone 12', price:2000, pic:pic.iphone12},
-    {id:2, name:'Iphone 7', price:3000},
-    {id:3, name:'Iphone 8', price:5000},
-    {id:4, name:'Iphone 9', price:6000},
+    {id:2, name:'Samsong Galaxy A03', price:3000, pic:pic.galaxyA03},
+    {id:3, name:'Samsong Galaxy A12', price:5000, pic:pic.galaxyA12},
+    {id:4, name:'Iphone 11', price:6000, pic:pic.iphone11},
+    {id:4, name:'Iphone 13 proMAX', price:6000, pic:pic.iphone13proMAX},
+    {id:4, name:'Redmi A9', price:6000, pic:pic.redmiA9},
 ]
 
 const productsReducer = (state = INITIAL_STATE, action) => {
