e267db6e5400ed8223e5d637336fe4bb21062de3
pooyan <thepooyan@gmail.com>
2022-03-24 06:20:51 +0430

    discount added

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Product.module.scss'
 
 const Product = (props) => {
-  return (
+    return (
     <div className={styles.card}>
         <img src={props.item.pic} alt={props.name} className={styles.img}/>
 
         <div className={styles.infoBox}>
             <h3 className={styles.title}> {props.item.name} </h3>
-            <p className={styles.price}>{props.item.price}</p>
+            <p className={styles.price}>{props.item.price}
+            {props.item.discount !== 0 ? <span className={styles.discount}>{props.item.discount}</span>: ''}
+            </p>
         </div>
 
     </div>
