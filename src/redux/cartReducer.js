d93fe603f671c4fe8fccfbe4ea92e0e22c42a4f4
pooyan <thepooyan@gmail.com>
2022-04-05 13:30:08 +0430

    clg removed

--- src/redux/cartReducer.js
+++ src/redux/cartReducer.js
@@ -5,7 +5,6 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case 'ADD_TO_CART':
             return [...state, action.product]
         case 'REMOVE_FROM_CART':
-            console.log(action.id)
             return state.filter(item=>item.id!=action.id)
         default: 
             return state
